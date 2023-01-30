9b3647b905bba794cfee0af11f6eee19207f2e46
Sidharrth Nagappan <sidharrth2002@gmail.com>
2023-01-30 22:40:03 +0100

    add comments to mcts2

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from quarto.objects import Quarto
 
 def main():
     wins = 0
-    for game in range(5):
+    for game in range(10):
         game = Quarto()
         game.set_players((FinalPlayer(game), RandomPlayer(game)))
         winner = game.run()
         print(f"main: Winner: player {winner}")
         if winner == 0:
             wins += 1
-    print(f"main: Wins: {wins}/5")
+    print(f"main: Wins: {wins}/10")
 
 if __name__ == "__main__":
     main()
